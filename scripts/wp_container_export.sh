0f6df84d706359216cc7640865864ce18383743f
Frag Spawn <frag.spawn@gmail.com>
2021-04-12 20:14:54 +1000

    debug restore script

--- scripts/wp_container_export.sh
+++ scripts/wp_container_export.sh
@@ -1,4 +1,6 @@
 #ensure to do this from the root of the project folder, while the microservices are running
 #!/bin/bash
-docker exec -it wp-cli /usr/local/bin/wp db export --path=/var/www/html - > sql/dump.sql
+ROOT_PW=$(grep DBRP .env | cut -d '=' -f2)
+
+docker exec -it wp-db /usr/bin/mysqldump -u root -p"$ROOT_PW" wp | grep -v "Using a password" > sql/dump.sql
 docker cp wp-cli:/var/www/html/wp-content/. wp-content/
